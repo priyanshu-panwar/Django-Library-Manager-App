e242e9258108d3f5bfc26255bfbdfb4600ef0b95
Priyanshu Panwar <priyanshu009ch@gmail.com>
2019-06-05 14:54:38 +0530

    deployed

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+django_heroku.settings(locals())
